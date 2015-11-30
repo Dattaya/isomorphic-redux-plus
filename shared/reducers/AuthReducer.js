13cab0edbbbd37796daec9cafd16de4af50a326a
Yaroslav Kiliba <om.dattaya@gmail.com>
2015-11-30 21:57:51 +0200

    Set another default value for `user`

--- shared/reducers/AuthReducer.js
+++ shared/reducers/AuthReducer.js
@@ -1,23 +1,23 @@
 import Immutable from 'immutable';
 
-const defaultState = Immutable.Map({loaded: false});
+const defaultState = Immutable.Map({loaded: false, user: null});
 
 export default function authReducer(state = defaultState, action = {}) {
   switch (action.type) {
     case 'LOAD':
-      return state.merge({loaded: true, user: action.res.data});
+      return state.merge({loaded: true, user: action.res.data || null});
 
     case 'LOGIN_REQUEST':
       return state.set('loggingIn', true);
 
     case 'LOGIN':
-      return state.merge({loggingIn: false, user: action.res.data, error: false});
+      return state.merge({loggingIn: false, user: action.res.data || null, error: false});
 
     case 'LOGIN_FAILURE':
-      return state.merge({loggingIn: false, user: '', error: true});
+      return state.merge({loggingIn: false, user: null, error: true});
 
     case 'LOGOUT':
-      return state.set('user', '');
+      return state.set('user', null);
 
     default:
       return state;
