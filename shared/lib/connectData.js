2b728455d9c08148f5e359785b6b959be6773fa3
Yaroslav Kiliba <om.dattaya@gmail.com>
2016-01-28 11:02:02 +0200

    Add `fetchDataDeferred` argument to the `connectData` decorator.

--- shared/lib/connectData.js
+++ shared/lib/connectData.js
@@ -1,32 +1,21 @@
 import React, { Component, PropTypes } from 'react';
 import hoistStatics                    from 'hoist-non-react-statics';
 
-const { shape, func, object } = PropTypes;
-
 /**
  * Should always be a top level (outermost) decorator because
  * on the server and on the client all `fetchData` properties are collected
  * to pass them to fetchComponentData.
  *
- * @param fetch
+ * @param fetchData
+ * @param fetchDataDeferred
  * @returns {Function}
  */
-export default (fetch) => {
+export default (fetchData, fetchDataDeferred) => {
   return WrappedComponent => {
     class FetchDataDecorator extends Component {
       static WrappedComponent = WrappedComponent;
-      static fetchData = fetch;
-
-      static propTypes = {
-        params: object
-      };
-
-      static contextTypes = {
-        store: shape({
-          dispatch: func.isRequired,
-          getState: func.isRequired
-        })
-      };
+      static fetchData = fetchData;
+      static fetchDataDeferred = fetchDataDeferred;
 
       render() {
         return <WrappedComponent {...this.props} />;
