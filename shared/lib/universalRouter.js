b73af877d980f9d9687151a59bf593ed4ee939e2
Yaroslav Kiliba <om.dattaya@gmail.com>
2016-01-30 20:55:45 +0200

    Prettify

--- shared/lib/universalRouter.js
+++ shared/lib/universalRouter.js
@@ -6,7 +6,7 @@ import {
 import { Provider }           from 'react-redux';
 
 import ErrorHandler       from 'lib/ErrorHandler';
-import fetchComponentData from './fetchComponentData';
+import fetchComponentData from 'lib/fetchComponentData';
 
 /**
  *
