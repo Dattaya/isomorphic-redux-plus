0a3e71d3e21ed56bc2ba33f6b213b6c020bced84
Yaroslav Kiliba <om.dattaya@gmail.com>
2016-05-26 13:11:16 +0300

    [server] Cache process.env for better perfomance

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// Speed things up on the server for libraries that unlike React do not cache `process.env`.
+// See https://github.com/facebook/react/issues/812.
+process.env = JSON.parse(JSON.stringify(process.env));
+
 require('babel-register')({});
 
 var server = require('./server').default;
