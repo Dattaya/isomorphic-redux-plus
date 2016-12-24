601685f860612079dbce25e12e97677fc38fde71
Kegan Myers <kegan@keganmyers.com>
2016-12-24 13:42:18 -0600

    add testing for fetchComponentData

--- app/lib/fetchComponentData.js
+++ app/lib/fetchComponentData.js
@@ -1,11 +1,5 @@
-export default function fetchComponentData(store, components, params) {
-  const needs = components.reduce((prev, current) => {
-    if (current && current.fetchData) {
-      prev.push(current.fetchData);
-    }
-    return prev;
-  }, []);
-  const promises = needs.map((need) => need(store.state, store.dispatch, params));
-
-  return Promise.all(promises);
+export default function fetchComponentData({ dispatch, state }, components, params) {
+  return Promise.all(components
+    .filter((component) => component && component.fetchData)
+    .map((component) => component.fetchData(state, dispatch, params)));
 }
